eed406ac12a5fe80d1496ae7bac619eb72d71805
shoeboxgames <shoe_wooberg@snobbywalrus.com>
2017-05-17 15:04:15 -0700

    DB creation Files
    Set up Entity Framework Code First database structure for the Movie
    class as well as created types for Genre and SubGenre

---
+++ MovieSite/GenreDefinitions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieSite
+{
+    public class GenreDefinitions
+    {
+       public enum Genre
+        {
+            Action,
+            Adventure,
+            Comedy,
+            Crime,
+            Drama,
+            Epic,
+            Horror,
+            Musical,
+            ScienceFiction,
+            War,
+            Western
+
+        };
+
+        public enum SubGenre
+        {
+            Biopic,
+            ChickFlick,
+            CourtroomDrama,
+            Mystery,
+            Disaster,
+            Fantasy,
+            Noir,
+            GuyFilms,
+            Melodrama,
+            RoadFilm,
+            Romance,
+            Sports,
+            SuperHeroes,
+            Supernatural,
+            Thriller,
+            Zombie
+
+        }
+    }
+}
---
+++ MovieSite/MovieDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using static MovieSite.GenreDefinitions;
+
+namespace MovieSite
+{
+   
+        public class Movie
+        {
+            public string Title { get; set; }
+            public Genre MainGenre { get; set; }
+            public SubGenre SubGenre { get; set; }
+            public string DirectorName { get; set; }
+            public DateTime DateReleased { get; set; }
+            public int Length { get; set; }
+            public string Description { get; set; }
+        }
+    
+}
