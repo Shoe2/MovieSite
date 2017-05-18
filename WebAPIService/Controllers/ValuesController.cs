7a61cdeab9037477b88d216966b5288fe9491315
shoeboxgames <shoe_wooberg@snobbywalrus.com>
2017-05-18 15:20:09 -0700

    Create WebAPI project and add refrences
    Added a new WebAPI project and created refrences needed for Entity
    Framework

---
+++ WebAPIService/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MovieSite;
+using System.Data.Entity;
+
+namespace WebAPIService.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET all movies to populate app
+        public IEnumerable<Movie> Get()
+        {            
+            return new List<Movie>
+            {
+                using (var db = new MovieContext())
+                {
+
+                }
+
+            };
+        }
+
+        // POST added movie to DB
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        
+    }
+}
