5aeed267482c046888ed378b4ace5b4e810089bf
shoeboxgames <shoe_wooberg@snobbywalrus.com>
2017-05-18 15:17:46 -0700

    Setting up refrence assemblies
    Adding in some refrence assemblies

--- MovieSite/MovieDB.cs
+++ MovieSite/MovieDB.cs
@@ -5,19 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using static MovieSite.GenreDefinitions;
+using System.Runtime.Remoting.Contexts;
 
 namespace MovieSite
 {
-   
-        public class Movie
+
+    public class Movie
+    {
+        public string Title { get; set; }
+        public Genre MainGenre { get; set; }
+        public List<SubGenre> SubGenre { get; set; }
+        public Director Director { get; set; }
+        public DateTime DateReleased { get; set; }
+        public int Length { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Director
+    {
+
+        public string DirectorFirstName { get; set; }
+        public string DirectorLastName { get; set; }
+
+    }
+
+    public class MovieContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Director> Directors { get; set; }
+    }
+
+    class AccessDB
+    {
+
+        public static void AddToDB ()
         {
-            public string Title { get; set; }
-            public Genre MainGenre { get; set; }
-            public List<SubGenre> SubGenre { get; set; }
-            public string DirectorName { get; set; }
-            public DateTime DateReleased { get; set; }
-            public int Length { get; set; }
-            public string Description { get; set; }
+            Movie newMovie = new Movie();
+            newMovie.Title = "test";
+            newMovie.MainGenre = Genre.Horror;
+            newMovie.SubGenre.Add(SubGenre.ChickFlick);
+            newMovie.DateReleased = DateTime.Now;
+            newMovie.Length = 66;
+            newMovie.Description = "FUUUUUUU";
+                        
+            using (var db = new MovieContext())
+            {
+                db.Movies.Add(newMovie);
+
+                
+                db.Directors.Add(newMovie.Director);
+                db.SaveChanges();
+            }
         }
+
+    }
     
 }
