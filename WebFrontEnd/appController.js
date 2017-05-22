3b02796976d694e4629e7a8d766937bb4872d111
shoeboxgames <shoe_wooberg@snobbywalrus.com>
2017-05-22 10:37:39 -0700

    angular app shell
    angular app shell

--- WebFrontEnd/appController.js
+++ WebFrontEnd/appController.js
@@ -1,1 +1,14 @@
-﻿
+﻿app.controller('movieAppController', ['$scope', '$http', function ($scope, '$http') {
+    $http.get('http://URL.com').
+        then(function (response) {
+            $scope.movies = response.data;
+        });
+    $scope.submit = function ()
+    {
+
+        $http.post('http://URL.com', data, config).then(successCallback, errorCallback);
+    }
+    
+
+
+}]);
