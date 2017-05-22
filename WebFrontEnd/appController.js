e87fbe7a66b02b8c930c6179078bf0e837cc7729
shoeboxgames <shoe_wooberg@snobbywalrus.com>
2017-05-22 12:18:47 -0700

    Add new movie popup implementation
    Add new movie popup implementation

--- WebFrontEnd/appController.js
+++ WebFrontEnd/appController.js
@@ -1,18 +1,23 @@
 ﻿var app = angular.module("movieApp", []);
-app.controller('movieAppController', ['$scope', '$http', function ($scope, '$http') {
-    $http.get('http://URL.com').
-        then(function (response) {
-            $scope.movies = response.data;
-        });
+app.controller('movieAppController', ['$scope', '$http', function ($scope, $http) {
+    $scope.addNewMovie = false;
+    //$http.get('http://URL.com').
+    //    then(function (response) {
+    //        $scope.movies = response.data;
+    //    });
     $scope.submit = function ()
     {
-
+        $scope.close();
         $http.post('http://URL.com', data, config).then(successCallback, errorCallback);
     }
     
     $scope.showAddMoviePopup = function ()
     {
         $scope.addNewMovie = true;
+    }
+
+    $scope.close = function () {
+        $scope.addNewMovie = false;
     }
 
 }]);
