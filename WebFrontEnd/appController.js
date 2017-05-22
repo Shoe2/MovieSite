985b8b12f7c07fb8d7b26021a10af5c62202a824
shoeboxgames <shoe_wooberg@snobbywalrus.com>
2017-05-22 12:46:03 -0700

    add movie popup styling and formatting
    add movie popup styling and formatting

--- WebFrontEnd/appController.js
+++ WebFrontEnd/appController.js
@@ -1,14 +1,20 @@
 ﻿var app = angular.module("movieApp", []);
 app.controller('movieAppController', ['$scope', '$http', function ($scope, $http) {
     $scope.addNewMovie = false;
-    //$http.get('http://URL.com').
-    //    then(function (response) {
-    //        $scope.movies = response.data;
-    //    });
+    //$scope.refreshMovies();
+    //$scope.refreshMovies = function () {
+    //    $http.get('http://URL.com').
+    //        then(function (response) {
+    //            $scope.movies = response.data;
+    //        });
+    //}
     $scope.submit = function ()
     {
         $scope.close();
-        $http.post('http://URL.com', data, config).then(successCallback, errorCallback);
+        $http.post('http://URL.com', data, config).
+            then($scope.refreshMovies(), function (response) {
+                alert(response);
+            });
     }
     
     $scope.showAddMoviePopup = function ()
