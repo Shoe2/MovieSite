c79df658f7690821eb2eb1890240f45523667612
shoeboxgames <shoe_wooberg@snobbywalrus.com>
2017-06-09 08:48:50 -0700

    setting up ng-model
    setting up the model to add new movies to the DB through the API

--- WebFrontEnd/appController.js
+++ WebFrontEnd/appController.js
@@ -2,40 +2,32 @@
 app.controller('movieAppController', ['$scope', '$http', function ($scope, $http) {
     $scope.addNewMovie = false;
     $scope.deleteMovie = false;
+    $scope.newMovie = {};
     
     $scope.refreshMovies = function () {
         $http.get('/movies/api/movies').
             then(function (response) {
                 $scope.movies = response.data.$values;
-            }, function (response) { console.log("ERROR:" + response.ExceptionMessage); });
+            }, function (response) { console.log("ERROR:" + response.data.ExceptionMessage); });
     };
 
     $scope.refreshMovies();
 
     $scope.submit = function () {
+        $http.post('/movies/api/Movies', $scope.newMovie).
+            then($scope.refreshMovies(),
+            function (response) { console.log("ERROR:" + response.data.ExceptionMessage); });
         $scope.close();
-        $http.post('/movies/api/SaveMovie', data, config).
-            then($scope.refreshMovies(), function (response) {
-                alert(response);
-            });
     };
     
     $scope.showPopup = function (movieID) {
-        if (movieID === '')
-        {
-            $scope.addNewMovie = true;
-        }
-            
-        else
-        {
-            $scope.deleteMovie = true;
-        }
+        if (movieID === '') {$scope.addNewMovie = true;}
+        else {$scope.deleteMovie = true;}
     };
 
     $scope.close = function () {
         $scope.addNewMovie = false;
         $scope.deleteMovie = false;
-
     };
 
 }]);
